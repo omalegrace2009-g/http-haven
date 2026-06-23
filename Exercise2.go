b50665931687399561990e0aea932d635086339d
oomale <omalegrace2009@gmail.com>
2026-06-23 11:17:21 +0100

    studying

--- Exercise2.go
+++ Exercise2.go
@@ -7,7 +7,7 @@ import (
 
 func HandleHello(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		http.Error(w, "Method Not Allowed!!", http.StatusMethodNotAllowed)
 		return
 	}
 	if r.URL.Path != "/hello" {
